4af1e9a5afa55e625f94c869932afd7bf1840b4f
Ivan Phelps <ivanniothemannio@gmail.com>
2019-10-28 21:02:54 -0500

    farmer promise and farmercow promise

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,14 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import cows from './components/cowList/cowList';
+import farmerList from './components/farmerList/farmerList';
+import farmerCowList from './components/farmerCowList/farmerCowList';
+
 
 const init = () => {
   cows.buildCows();
+  farmerList.buildFarmers();
+  farmerCowList.buildFarmerCows();
 };
 
 init();
